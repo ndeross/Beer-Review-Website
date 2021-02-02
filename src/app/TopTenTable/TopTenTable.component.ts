a484d9692522f8ce88f9eb84458889580bfa3704
Nicholas DeRoss <56231776+ndeross@users.noreply.github.com>
2021-02-02 10:34:04 -0500

    Continuing work on main page

--- src/app/TopTenTable/TopTenTable.component.ts
+++ src/app/TopTenTable/TopTenTable.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
 import { Beer } from '../beer';
 import { BEERS } from '../mockbeers';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-TopTenTable',
   templateUrl: './TopTenTable.component.html',
   styleUrls: ['./TopTenTable.component.css'],
 })
-export class TopTenTable implements OnInit {
+export class TopTenTable {
   beers = BEERS;
-  selectedBeer: Beer;
+  sortedData = this.beers;
 
-  constructor() {}
-
-  ngOnInit() {}
-
-  onSelect(beer: Beer): void {
-    this.selectedBeer = beer;
+  constructor() {
+    this.sortedData = this.beers.slice();
   }
+
+  sortData(sort: Sort) {
+    const data = this.beers.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'desc';
+      switch (sort.active) {
+        case 'rating':
+          return compare(a.rating, b.rating, isAsc);
+
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
