312ca6d755e61730199814c73ba555c804447bb0
Paul Schick <paulschick19@gmail.com>
2021-02-16 21:53:23 -0500

    Moved Header/Footer to App Component
    Rather than importing into each template individually, I wrapped them around the router outlet to repeat repetition
    One-cases where they are not used can be handled individually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { TopTenTable } from './TopTenTable/TopTenTable.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ReviewPageComponent } from './review-page/review-page.component';
