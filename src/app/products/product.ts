ef11f5a3004aaa6f1fbdd8e47f23f72f2fad0482
bwatto <bwatto@ceiamerica.com>
2021-02-25 11:33:06 -0500

    updating product info

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,10 +1,7 @@
 export interface IProduct {
   productId: number;
   productName: string;
-  productCode: string;
-  releaseDate: string;
   price: number;
   description: string;
-  starRating: number;
   imageUrl: string;
 }
